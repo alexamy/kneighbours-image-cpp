ca6e4b544f68cddbe611a1d6107f988066ce99d3
Alexander Chaschin <achasch@gmail.com>
2020-08-21 01:00:36 +0300

    add proper build flags

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "png++/png.hpp"
 
 using namespace std;
 
 int main(int argc, const char *argv[]) {
+  png::image< png::rgb_pixel > image("input.png");
+  image.write("output.png");
   return 0;
 }
