eac265f042bc8a43ac6787bcd277f150740dcb4f
Alexander Chaschin <achasch@gmail.com>
2020-08-21 02:24:08 +0300

    add pixel iterator

--- main.cpp
+++ main.cpp
@@ -2,9 +2,21 @@
 #include "png++/png.hpp"
 
 using namespace std;
+using namespace png;
 
-int main(int argc, const char *argv[]) {
-  png::image< png::rgb_pixel > image("input.png");
+int main(int argc, const char *argv[])
+{
+  image<rgb_pixel> image("input.png");
+
+  for(int i = 0; i < image.get_width(); i++)
+  {
+    for(int j = 0; j < image.get_height(); j++)
+    {
+      rgb_pixel pixel = image.get_pixel(i, j);
+    }
+  }
+
+
   image.write("output.png");
   return 0;
 }
