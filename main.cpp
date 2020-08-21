3bd18d8cf13ce1608d5434cd47d0de23cef91cd2
Alexander Chaschin <achasch@gmail.com>
2020-08-21 14:18:49 +0300

    add comments

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@ using namespace png;
 
 int main(int argc, const char *argv[])
 {
+  // load image
   image<rgb_pixel> image_original("input.png");
   uint_32 width = image_original.get_width();
   uint_32 height = image_original.get_height();
+  uint_32 size = width * height;
 
-  vector<rgb_pixel> original(width * height);
+  // get pixels of input image
+  vector<rgb_pixel> original(size);
   for(int i = 0; i < width; i++)
   {
     for(int j = 0; j < height; j++)
@@ -19,15 +22,21 @@ int main(int argc, const char *argv[])
     }
   }
 
+  // simplify pixels
+  vector<rgb_pixel> simplified(original);
+
+  // write simplified image
   image<rgb_pixel> image_simplified(width, height);
   size_t x, y;
-  for(int i = 0; i < original.size(); i++)
+  for(int i = 0; i < simplified.size(); i++)
   {
     x = i % width;
     y = (i - x) / width;
-    image_simplified.set_pixel(x, y, original[i]);
+    image_simplified.set_pixel(x, y, simplified[i]);
   }
 
   image_simplified.write("output.png");
+
+  // exit
   return 0;
 }
