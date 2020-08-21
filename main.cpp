ab5c5cf21a7fe1c53df710dcb2adb63b2a9f4cdf
Alexander Chaschin <achasch@gmail.com>
2020-08-21 03:20:35 +0300

    copy pixels between images

--- main.cpp
+++ main.cpp
@@ -6,16 +6,29 @@ using namespace png;
 
 int main(int argc, const char *argv[])
 {
-  image<rgb_pixel> image("input.png");
+  image<rgb_pixel> image_original("input.png");
+  uint_32 width = image_original.get_width();
+  uint_32 height = image_original.get_height();
 
-  for(int i = 0; i < image.get_width(); i++)
+  vector<vector<rgb_pixel>> original(width);
+  for(int i = 0; i < width; i++)
   {
-    for(int j = 0; j < image.get_height(); j++)
+    original[i] = vector<rgb_pixel>(height);
+    for(int j = 0; j < height; j++)
     {
-      rgb_pixel pixel = image.get_pixel(i, j);
+      original[i][j] = image_original.get_pixel(i, j);
     }
   }
 
-  image.write("output.png");
+  image<rgb_pixel> image_simplified(width, height);
+  for(int i = 0; i < width; i++)
+  {
+    for(int j = 0; j < height; j++)
+    {
+      image_simplified.set_pixel(i, j, original[i][j]);
+    }
+  }
+
+  image_simplified.write("output.png");
   return 0;
 }
