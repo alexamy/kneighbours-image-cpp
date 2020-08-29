e829847542e3c4e1f08c7b7fe9fb8c7319361d4e
Alexander Chaschin <achasch@gmail.com>
2020-08-29 16:15:35 +0300

    extract libpng wrappers

---
+++ libs/libpng_wrapper.h
@@ -0,0 +1,28 @@
+#ifndef LIBPNG_WRAPPER_H
+#define LIBPNG_WRAPPER_H
+
+#include <png.h>
+
+void read_png_file(
+  char *filename,
+  png_bytep **row_pointers,
+  png_uint_32 *width,
+  png_uint_32 *height,
+  png_byte *color_type,
+  png_byte *bit_depth
+);
+
+void write_png_file(
+  char *filename,
+  png_bytep *row_pointers,
+  png_uint_32 width,
+  png_uint_32 height
+);
+
+void process_png_file(
+  png_bytep *row_pointers,
+  png_uint_32 width,
+  png_uint_32 height
+);
+
+#endif
